0c7a1df76079b8013e256f43d5610bff5c4c25b1
Bruno Koster <bruno10762@gmail.com>
2020-11-10 16:59:30 -0300

    create aula02

---
+++ WEB2/Aula02/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $nome = "Bruno";
+    $sobrenome = "Koster";
+    $idade = 16;
+
+    echo '<h1>Meu nome é:'.$nome.' '.$sobrenome.'</h1>';
+    echo '<p style="font-size: 30px; color: blue;">Idade: '.$idade.'</p>';
+
+?>
