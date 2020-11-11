f3339bd5d88023d5878c607120c0c37a80d41f56
Bruno Koster <bruno10762@gmail.com>
2020-11-11 20:14:08 -0300

    update class03

---
+++ WEB2/class03/mathExample.php
@@ -0,0 +1,10 @@
+<?php
+
+//  example to calculate the sum of two values
+    $value1 = 10;
+    $value2 = 5;
+
+    $result = $value1 + $value2;
+    echo '<h3>The sum result is: '.$result.'</h3>';
+
+?>
